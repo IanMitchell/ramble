ffeb18ffc3b4c3f116351720ced14d49aff664af
Ian Mitchell <ian.mitchell@hey.com>
2020-12-26 23:46:03 -0800

    Add Next Auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const options = {
+  providers: [
+    Providers.Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/admin',
+  },
+  adapter: Adapters.Prisma.Adapter({ prisma }),
+};
+
+export default (req, res) => NextAuth(req, res, options);
