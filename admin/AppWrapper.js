d86513ace09e819a23f5d278d2fa9402cf4a8e8f
Ian Mitchell <ian.mitchell@hey.com>
2021-02-11 15:23:25 -0800

    Add Admin Panel Wrapper

---
+++ admin/AppWrapper.js
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router";
+import { SWRConfig } from "swr";
+import { Provider as AuthProvider } from "next-auth/client";
+import NotificationsContainer from "./components/notifications/NotificationsContainer";
+
+async function fetcher(...args) {
+  const response = await fetch(...args);
+  const data = await response.json();
+
+  if (!response.ok) {
+    const error = new Error("Error Fetching Data");
+    error.status = response.status;
+
+    if (data?.errors?.length === 1) {
+      error.name = data.errors[0].name;
+      error.message = data.errors[0].message;
+    } else {
+      error.name = "Encountered Errors";
+      error.entries = data.errors;
+    }
+
+    throw error;
+  }
+
+  return data;
+}
+
+export default function AdminConditionalWrapper({ pageProps, children }) {
+  const router = useRouter();
+
+  if (router.pathname.startsWith("/admin")) {
+    return (
+      <AuthProvider session={pageProps.session}>
+        <NotificationsContainer>
+          <SWRConfig value={{ fetcher }}>{children}</SWRConfig>
+        </NotificationsContainer>
+      </AuthProvider>
+    );
+  }
+
+  return children;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,51 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
-
 import { Fragment, useEffect } from "react";
-import { Provider as AuthProvider } from "next-auth/client";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import * as Fathom from "fathom-client";
-import { SWRConfig } from "swr";
-import NotificationsContainer from "../admin/components/notifications/NotificationsContainer";
+import AdminAppWrapper from "../admin/AppWrapper";
 import "tailwindcss/tailwind.css";
-
-async function fetcher(...args) {
-  const response = await fetch(...args);
-  const data = await response.json();
-
-  if (!response.ok) {
-    const error = new Error("Error Fetching Data");
-    error.status = response.status;
-
-    if (data?.errors?.length === 1) {
-      error.name = data.errors[0].name;
-      error.message = data.errors[0].message;
-    } else {
-      error.name = "Encountered Errors";
-      error.entries = data.errors;
-    }
-
-    throw error;
-  }
-
-  return data;
-}
-
-function AdminConditionalWrapper({ pageProps, children }) {
-  const router = useRouter();
-
-  if (router.pathname.startsWith("/admin")) {
-    return (
-      <AuthProvider session={pageProps.session}>
-        <NotificationsContainer>
-          <SWRConfig value={{ fetcher }}>{children}</SWRConfig>
-        </NotificationsContainer>
-      </AuthProvider>
-    );
-  }
-
-  return children;
-}
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -74,9 +33,9 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
-      <AdminConditionalWrapper pageProps={pageProps}>
+      <AdminAppWrapper pageProps={pageProps}>
         <Component {...pageProps} />
-      </AdminConditionalWrapper>
+      </AdminAppWrapper>
     </Fragment>
   );
 }
